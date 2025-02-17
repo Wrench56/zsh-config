4a580ab7728481176a89ee3438c9f7eb3edb9dcd
Mark Devenyi <markdevenyidev@gmail.com>
2025-02-16 19:09:20 -0500

    fix: make `install.sh` POSIX compliant

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ cp shell/aliases ~/.local/aliases
 cp shell/keybinds ~/.local/keybinds
 
 # Call distro specific install
-source /etc/os-release
+. /etc/os-release
 
 case "$NAME" in
     "Arch Linux")
-        printf "[    ] Selecting Arch Linux install"
+        printf "[    ] Selecting Arch Linux install\n"
         chmod +x distros/install-arch.sh
-        source distros/install-arch.sh
+        . distros/install-arch.sh
         ;;
     *)
-        printf "[FAIL] No valid distro specific installation script found!"
+        printf "[FAIL] No valid distro specific installation script found!\n"
 esac
 
