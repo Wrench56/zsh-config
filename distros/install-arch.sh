6a8f7dd0d06af3031734c02106e2b68c3093a8b8
Mark Devenyi <markdevenyidev@gmail.com>
2025-02-16 19:15:47 -0500

    refactor(plugins): install common plugins in `install.sh`
    Non-distro-specific plugins are going to be installed in `install.sh`.

--- distros/install-arch.sh
+++ distros/install-arch.sh
@@ -10,7 +10,3 @@ paru -S --noconfirm --needed zsh-theme-powerlevel10k-git
 
 sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 
-sudo git clone https://github.com/jirutka/zsh-shift-select /usr/share/zsh/plugins/zsh-shift-select
-
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh/plugins/zsh-autosuggestions
-
--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ cp p10k.zsh ~/.config/zsh/p10k.zsh
 cp shell/aliases ~/.local/aliases
 cp shell/keybinds ~/.local/keybinds
 
+# Install common plugins
+printf "[    ] Installing common plugins\n"
+sudo git clone https://github.com/jirutka/zsh-shift-select /usr/share/zsh/plugins/zsh-shift-select
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh/plugins/zsh-autosuggestions
+
 # Call distro specific install
 . /etc/os-release
 
